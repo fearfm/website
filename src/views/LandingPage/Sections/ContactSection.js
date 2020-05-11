e4be24b92536796db1915efe95e541f9f3c5a52a
Pim Jansen <pjansen@senet.nl>
2020-05-11 17:01:59 +0200

    Initial

---
+++ src/views/LandingPage/Sections/ContactSection.js
@@ -0,0 +1,69 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ContactSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem cs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Contact us</h2>
+          <h4 className={classes.description}>
+            In case you want to reach out to us, you can use below form. Please note that it can
+            take a few business days before we can send you an actual response.
+          </h4>
+          <form>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Your Name"
+                  id="name"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Your Email"
+                  id="email"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                />
+              </GridItem>
+              <CustomInput
+                labelText="Your Message"
+                id="message"
+                formControlProps={{
+                  fullWidth: true,
+                  className: classes.textArea
+                }}
+                inputProps={{
+                  multiline: true,
+                  rows: 5
+                }}
+              />
+              <GridItem xs={12} sm={12} md={6}>
+                <Button color="primary">Send Message</Button>
+              </GridItem>
+            </GridContainer>
+          </form>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
