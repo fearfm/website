ac55aa465de087ee17c57f56c361cbcc2bbf3d20
Tim Zwanenberg <tim@acato.nl>
2020-05-13 09:59:28 +0200

    Configure typescript and create context to retrieve playlist info

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { HotModuleReplacementPlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
+    devtool: "source-map",
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(ts|js)x?$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
                 use: [
                     {
-                        loader: "html-loader"
+                        loader: "babel-loader"
                     }
                 ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
             }
-
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+        new HotModuleReplacementPlugin(),
     ],
 };
 
