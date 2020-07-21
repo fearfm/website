03300361f98728a14a241550d0a0ee727755ab6d
Pim Jansen <pjansen@senet.nl>
2020-07-21 17:14:38 +0200

    Indented file

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,46 +1,46 @@
 declare module "*.svg" {
-  import {AnyStyledComponent} from "styled-components";
+    import {AnyStyledComponent} from "styled-components";
     const value: string;
-  export const ReactComponent: React.Component<React.SVGAttributes<SVGElement>> & AnyStyledComponent
-  export default value
+    export const ReactComponent: React.Component<React.SVGAttributes<SVGElement>> & AnyStyledComponent
+    export default value
 }
 
 declare module "*.png" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.jpg" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.jpeg" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.xml" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.webmanifest" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.eot" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.ttf" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
 
 declare module "*.woff" {
-  const value: never;
-  export default value;
+    const value: never;
+    export default value;
 }
