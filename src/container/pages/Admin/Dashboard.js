8d6f2b6b4c373072724da4588140b2e36f12772c
Pim Jansen <pjansen@senet.nl>
2020-09-27 19:19:53 +0200

    Fixed stupid lint issues

--- src/container/pages/Admin/Dashboard.js
+++ src/container/pages/Admin/Dashboard.js
@@ -1,10 +1,6 @@
 import React from "react"
-import {withAuthenticationRequired} from "@auth0/auth0-react";
+import { withAuthenticationRequired } from "@auth0/auth0-react"
 
 export const Dashboard = withAuthenticationRequired(() => {
-  return (
-    <>
-      todo
-    </>
-  )
-});
+  return <>todo</>
+})
