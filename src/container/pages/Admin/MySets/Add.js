53f2fa200201937713f147cf2936367a5849fd7d
Pim Jansen <pjansen@senet.nl>
2020-10-02 15:18:47 +0200

    Fixed linter

--- src/container/pages/Admin/MySets/Add.js
+++ src/container/pages/Admin/MySets/Add.js
@@ -1,64 +1,64 @@
 import React from "react"
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom"
 import { withAuthenticationRequired } from "@auth0/auth0-react"
 import { Button, Grid, Typography, TextField, Divider } from "@material-ui/core"
-import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
-import { useForm } from "react-hook-form";
+import MuiAlert from "@material-ui/lab/Alert"
+import { makeStyles } from "@material-ui/core/styles"
+import { useForm } from "react-hook-form"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-}));
+}))
 
 export const Add = withAuthenticationRequired(() => {
-  const [error, setError] = React.useState(null);
+  const [error, setError] = React.useState(null)
   const [formFields, setFormFields] = React.useState(
-    [1,2,3,4]
+    [1, 2, 3, 4]
     // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
-  );
-  const classes = useStyles();
-  const history = useHistory();
-  const { register, handleSubmit, watch, errors } = useForm();
+  )
+  const classes = useStyles()
+  const history = useHistory()
+  const { register, handleSubmit, watch, errors } = useForm()
 
   const onSubmit = async (data) => {
-    const filledFields = formFields.filter(id => {
-      if (data['artist'+id] !== '') {
-        return id;
+    const filledFields = formFields.filter((id) => {
+      if (data["artist" + id] !== "") {
+        return id
       }
-    });
-    const formattedTrackFields = filledFields.map(id => {
+    })
+    const formattedTrackFields = filledFields.map((id) => {
       return {
-        artist: data['artist'+id],
-        title: data['title'+id],
-        image: data['image'+id],
-        starttime: data['starttime'+id],
+        artist: data["artist" + id],
+        title: data["title" + id],
+        image: data["image" + id],
+        starttime: data["starttime" + id],
       }
-    });
+    })
 
-    const res = await fetch(process.env.REACT_APP_TRACK_HOST + '/tracklist', {
-      method: 'POST',
+    const res = await fetch(process.env.REACT_APP_TRACK_HOST + "/tracklist", {
+      method: "POST",
       body: JSON.stringify({
-        artist: data['set-artist'],
-        title: data['set-title'],
-        reference: data['set-identifier'],
+        artist: data["set-artist"],
+        title: data["set-title"],
+        reference: data["set-identifier"],
         tracks: formattedTrackFields,
-      })
-    });
+      }),
+    })
     res
       .json()
-      .then(res => {
+      .then((res) => {
         if (res.status === 200) {
-          history.push('/admin/my-sets');
+          history.push("/admin/my-sets")
         } else {
-          setError(res.message);
+          setError(res.message)
         }
       })
-      .catch(err => {
-        console.error(err);
-      });
-  };
+      .catch((err) => {
+        console.error(err)
+      })
+  }
 
   // new Dropzone('#file-upload', {
   //   url: '/tracklist/chunk',
@@ -74,11 +74,11 @@ export const Add = withAuthenticationRequired(() => {
   //   }
   // });
 
-  const formFieldTemplate = formFields.map(id => {
-    const artistIdentifier = 'artist'+id;
-    const titleIdentifier = 'title'+id;
-    const imageIdentifier = 'image'+id;
-    const startIdentifier = 'starttime'+id;
+  const formFieldTemplate = formFields.map((id) => {
+    const artistIdentifier = "artist" + id
+    const titleIdentifier = "title" + id
+    const imageIdentifier = "image" + id
+    const startIdentifier = "starttime" + id
     return (
       <React.Fragment key={id}>
         <Grid item xs={2}>
@@ -86,7 +86,7 @@ export const Add = withAuthenticationRequired(() => {
         </Grid>
         <Grid item xs={5}>
           <TextField
-            label='Artist'
+            label="Artist"
             fullWidth
             name={artistIdentifier}
             id={artistIdentifier}
@@ -97,7 +97,7 @@ export const Add = withAuthenticationRequired(() => {
         </Grid>
         <Grid item xs={5}>
           <TextField
-            label='Title'
+            label="Title"
             fullWidth
             name={titleIdentifier}
             id={titleIdentifier}
@@ -109,7 +109,7 @@ export const Add = withAuthenticationRequired(() => {
         <Grid item xs={2} />
         <Grid item xs={5}>
           <TextField
-            label='Image'
+            label="Image"
             fullWidth
             name={imageIdentifier}
             id={imageIdentifier}
@@ -120,7 +120,7 @@ export const Add = withAuthenticationRequired(() => {
         </Grid>
         <Grid item xs={5}>
           <TextField
-            label='Start time'
+            label="Start time"
             fullWidth
             name={startIdentifier}
             id={startIdentifier}
@@ -131,19 +131,17 @@ export const Add = withAuthenticationRequired(() => {
         </Grid>
       </React.Fragment>
     )
-  });
+  })
 
-  const setArtistIdentifier = 'set-artist';
-  const setTitleIdentifier = 'set-title';
-  const setReferenceIdentifier = 'set-identifier';
+  const setArtistIdentifier = "set-artist"
+  const setTitleIdentifier = "set-title"
+  const setReferenceIdentifier = "set-identifier"
 
-  const errorMessage = error ? <MuiAlert severity="warning">{error}</MuiAlert> : null;
+  const errorMessage = error ? <MuiAlert severity="warning">{error}</MuiAlert> : null
   return (
     <>
-      <Typography variant="h4">
-        Add a new set
-      </Typography>
-      { errorMessage }
+      <Typography variant="h4">Add a new set</Typography>
+      {errorMessage}
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container className={classes.root}>
           <Grid item xs={12}>
@@ -154,41 +152,48 @@ export const Add = withAuthenticationRequired(() => {
           </Grid>
           <Grid item xs={5}>
             <TextField
-              label='Artist'
+              label="Artist"
               fullWidth
               name={setArtistIdentifier}
               id={setArtistIdentifier}
-              inputRef={register({ required: 'This field is required' })}
+              inputRef={register({ required: "This field is required" })}
               error={errors[setArtistIdentifier] !== undefined}
-              helperText={errors[setArtistIdentifier] && errors[setArtistIdentifier].message}
+              helperText={
+                errors[setArtistIdentifier] && errors[setArtistIdentifier].message
+              }
             />
           </Grid>
           <Grid item xs={5}>
             <TextField
-              label='Title'
+              label="Title"
               fullWidth
               name={setTitleIdentifier}
               id={setTitleIdentifier}
-              inputRef={register({ required: 'This field is required' })}
+              inputRef={register({ required: "This field is required" })}
               error={errors[setTitleIdentifier] !== undefined}
-              helperText={errors[setTitleIdentifier] && errors[setTitleIdentifier].message}
+              helperText={
+                errors[setTitleIdentifier] && errors[setTitleIdentifier].message
+              }
             />
           </Grid>
           <Grid item xs={2} />
           <Grid item xs={10}>
             <TextField
-              label='Reference'
+              label="Reference"
               fullWidth
               name={setReferenceIdentifier}
               id={setReferenceIdentifier}
-              inputRef={register({ required: 'This field is required' })}
+              inputRef={register({ required: "This field is required" })}
               error={errors[setReferenceIdentifier] !== undefined}
-              helperText={errors[setReferenceIdentifier] && errors[setReferenceIdentifier].message}
+              helperText={
+                errors[setReferenceIdentifier] &&
+                errors[setReferenceIdentifier].message
+              }
             />
           </Grid>
           {formFieldTemplate}
         </Grid>
-        <Button type='submit'>Save</Button>
+        <Button type="submit">Save</Button>
       </form>
     </>
   )
