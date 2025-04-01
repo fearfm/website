6159ff4c0597b90b556826d59cb3f9ca641ba868
Pim Jansen <pim.jansen@topicus.nl>
2025-04-01 21:08:54 +0200

    Restart unless-stopped

--- app/socket/ws.client.ts
+++ app/socket/ws.client.ts
@@ -1,10 +1,7 @@
 import { Socket, io } from "socket.io-client";
 
 export function connect(): Socket {
-  return io(
-    import.meta.env.NOWPLAYING_HOST,
-    {
-      transports: ["websocket"],
-    },
-  );
+  return io(import.meta.env.VITE_NOWPLAYING_HOST, {
+    transports: ["websocket"],
+  });
 }
