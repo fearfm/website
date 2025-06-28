59d777d4c8fdd64f8c028da690433c033838d919
Pim Jansen <pim.jansen@topicus.nl>
2025-06-28 14:07:41 +0200

    Disabled nowplaying during migration

--- app/routes/nowplaying.tsx
+++ app/routes/nowplaying.tsx
@@ -2,10 +2,10 @@ import { Icon } from "@iconify/react";
 import playArrowRounded from "@iconify/icons-material-symbols/play-arrow-rounded";
 import pauseRounded from "@iconify/icons-material-symbols/pause-rounded";
 import autoPauseRounded from "@iconify/icons-material-symbols/autopause-rounded";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PlayerContext } from "~/components/PlayerProvider";
 import useDeviceDetect from "~/hooks/useDeviceDetect";
-import { wsContext } from "~/socket/ws.context";
+// import { wsContext } from "~/socket/ws.context";
 import defaultArtwork from "~/assets/artwork-default-new.jpg";
 import { Slider, Tooltip } from "radix-ui";
 import VolumeHigh from "~/components/Icons/VolumeHight";
@@ -24,8 +24,8 @@ interface INowplaying {
 export default function Nowplaying() {
   const player = useContext(PlayerContext);
   const { isMobile } = useDeviceDetect();
-  const socket = useContext(wsContext);
-  const [volumeTooltipOpen,setVolumeTooltipOpen] = useState<boolean>(false);
+  // const socket = useContext(wsContext);
+  const [volumeTooltipOpen, setVolumeTooltipOpen] = useState<boolean>(false);
   const [nowplaying, setNowplaying] = useState<INowplaying>({
     artist: "Be Proud and Listen Loud",
     title: "Fear.FM",
@@ -35,17 +35,42 @@ export default function Nowplaying() {
 
   const getVolumeIcon = () => {
     if (player.muted) {
-      return <VolumeMute className={"cursor-pointer"} onClick={() => player.setMuted(false)}/>;
+      return (
+        <VolumeMute
+          className={"cursor-pointer"}
+          onClick={() => player.setMuted(false)}
+        />
+      );
     } else if (player.volume < 1) {
-      return <VolumeOff className={"cursor-pointer"} onClick={() => player.setMuted(true)} />;
+      return (
+        <VolumeOff
+          className={"cursor-pointer"}
+          onClick={() => player.setMuted(true)}
+        />
+      );
     } else if (player.volume < 30) {
-      return <VolumeLow className={"cursor-pointer"} onClick={() => player.setMuted(true)} />;
+      return (
+        <VolumeLow
+          className={"cursor-pointer"}
+          onClick={() => player.setMuted(true)}
+        />
+      );
     } else if (player.volume < 80) {
-      return <VolumeMid className={"cursor-pointer"} onClick={() => player.setMuted(true)} />;
+      return (
+        <VolumeMid
+          className={"cursor-pointer"}
+          onClick={() => player.setMuted(true)}
+        />
+      );
     } else {
-      return <VolumeHigh className={"cursor-pointer"} onClick={() => player.setMuted(true)} />;
+      return (
+        <VolumeHigh
+          className={"cursor-pointer"}
+          onClick={() => player.setMuted(true)}
+        />
+      );
     }
-  }
+  };
 
   const getPlayerOverlayIcon = () => {
     if (player.loading) {
@@ -70,24 +95,24 @@ export default function Nowplaying() {
     );
   };
 
-  useEffect(() => {
-    if (!socket) return;
-    socket.on("nowplaying update", (res) => {
-      setNowplaying({
-        artist: res.track.artist,
-        title: res.track.title,
-        tracklist: res.tracklist.title,
-        image: res.track.image ?? defaultArtwork,
-      });
-      document.title = `${res.track.artist} - ${res.track.title}`;
-    });
-    return () => {
-      socket.off("nowplaying update");
-    };
-  }, [socket]);
+  // useEffect(() => {
+  //   if (!socket) return;
+  //   socket.on("nowplaying update", (res) => {
+  //     setNowplaying({
+  //       artist: res.track.artist,
+  //       title: res.track.title,
+  //       tracklist: res.tracklist.title,
+  //       image: res.track.image ?? defaultArtwork,
+  //     });
+  //     document.title = `${res.track.artist} - ${res.track.title}`;
+  //   });
+  //   return () => {
+  //     socket.off("nowplaying update");
+  //   };
+  // }, [socket]);
 
   useEffect(() => {
-    const volume = parseInt(window.localStorage.getItem('volume') ?? "80");
+    const volume = parseInt(window.localStorage.getItem("volume") ?? "80");
     player.setVolume(volume);
   }, []);
 
@@ -100,7 +125,13 @@ export default function Nowplaying() {
           src={nowplaying.image}
         />
         <div className={"flex h-full flex-col justify-center items-center"}>
-          <h1 className={"text-xl px-2 mt-3 mb-2 text-white break-words line-clamp-2"}>{nowplaying.title}</h1>
+          <h1
+            className={
+              "text-xl px-2 mt-3 mb-2 text-white break-words line-clamp-2"
+            }
+          >
+            {nowplaying.title}
+          </h1>
           <h2 className={"text-sm"}>{nowplaying.artist}</h2>
           <div className={"text-6xl text-white"}>{getPlayerOverlayIcon()}</div>
         </div>
@@ -124,14 +155,16 @@ export default function Nowplaying() {
       <div>
         <h1 className={"text-4xl mb-2 text-white"}>{nowplaying.title}</h1>
         <h2 className={"text-2xl"}>{nowplaying.artist}</h2>
-        <span className={"text-xs bg-slate-800 rounded-md px-2 py-1"}>{nowplaying.tracklist}</span>
+        <span className={"text-xs bg-slate-800 rounded-md px-2 py-1"}>
+          {nowplaying.tracklist}
+        </span>
         <div className={"flex items-center mt-4 gap-4"}>
           {getVolumeIcon()}
           <Slider.Root
             className="relative flex h-5 w-96 touch-none select-none items-center"
             onValueChange={(volume) => {
               player.setVolume(volume[0]);
-              window.localStorage.setItem('volume', volume[0].toString())
+              window.localStorage.setItem("volume", volume[0].toString());
             }}
             value={[player.volume]}
             max={100}
@@ -140,10 +173,7 @@ export default function Nowplaying() {
             <Slider.Track className="relative h-[3px] grow rounded-full bg-slate-700">
               <Slider.Range className="absolute h-full rounded-full bg-white hover:bg-slate-200 cursor-pointer" />
             </Slider.Track>
-            <Tooltip.Root
-              delayDuration={0}
-              open={volumeTooltipOpen}
-            >
+            <Tooltip.Root delayDuration={0} open={volumeTooltipOpen}>
               <Tooltip.Trigger asChild>
                 <Slider.Thumb
                   className="cursor-pointer block size-5 rounded-[10px] bg-white hover:bg-slate-200 shadow-[0_2px_10px] shadow-blackA4 hover:bg-violet3 focus:shadow-[0_0_0_5px] focus:shadow-blackA5 focus:outline-none"
@@ -154,8 +184,11 @@ export default function Nowplaying() {
                 />
               </Tooltip.Trigger>
               <Tooltip.Portal>
-                <Tooltip.Content side={"bottom"} forceMount={true}
-                  className="select-none rounded bg-white px-4 py-2 leading-none will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"                  sideOffset={5}
+                <Tooltip.Content
+                  side={"bottom"}
+                  forceMount={true}
+                  className="select-none rounded bg-white px-4 py-2 leading-none will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
+                  sideOffset={5}
                 >
                   {player.volume} %
                   <Tooltip.Arrow className="fill-white" />
